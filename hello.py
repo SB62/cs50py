58c629c50cb02512dfb941ff91d054bcab40e652
K. Ricks <SB62@users.noreply.github.com>
2024-04-21 22:40:37 -0400

    'first' python program code along

---
+++ hello.py
@@ -0,0 +1,8 @@
+# Ask user for their name
+name = input("What's your name? ").strip().title()
+
+# Strip whitespace and capitalize first letters of Input
+name = name.strip().title()
+
+# Say hello to user
+print(f"hello, {name}")
