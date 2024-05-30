fe04e6f55bcd90fc1d4160edc38c226e1e0f8ace
K. Ricks <SB62@users.noreply.github.com>
2024-05-30 16:14:07 -0400

    Problem Set 2 camelCase submission

---
+++ problem_set_2/camel.py
@@ -0,0 +1,15 @@
+def main():
+    user_string = input("What's your variable name?")
+    print(camel_to_snake(user_string))
+
+
+def camel_to_snake(camel_case):
+  snake_case = ""
+  for char in camel_case:
+    if char.isupper() and snake_case:
+      snake_case += "_"
+    snake_case += char.lower()
+  return snake_case
+
+if __name__ == "__main__":
+    main()
